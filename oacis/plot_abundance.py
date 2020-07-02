7d56c224cffbf4f20453c2205d9dd69629f36269
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-07-02 11:05:47 +0900

    make summarized plot

--- oacis/plot_abundance.py
+++ oacis/plot_abundance.py
@@ -1,5 +1,6 @@
 import pprint
 import glob
+import re
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -28,6 +29,31 @@ def make_plot(datfile, names):
     plt.legend()
     plt.savefig(datfile.replace(".dat", ".png"))
 
+def make_summarized_plot(datfile, names):
+    plt.clf()
+    dat = np.loadtxt(datfile)
+    plt.xlabel('benefit')
+    plt.ylabel('abundance')
+    plt.ylim(0.0, 1.0)
+    legends = ["m=1 defensible", "m=1 efficient", "m=1 other"]
+    plot_data = [np.zeros_like(dat[:,1]), np.zeros_like(dat[:,1]), np.zeros_like(dat[:,1])]
+    for i,name in enumerate(names):
+        if re.search(r'/\d_D$', name):
+            plot_data[0] += dat[:,i+1]
+        elif re.search(r'/\d_E$', name):
+            plot_data[1] += dat[:,i+1]
+        elif re.search(r'/\d$', name):
+            plot_data[2] += dat[:,i+1]
+        else:
+            plot_data.append(dat[:,i+1])
+            legends.append(name)
+    for x,n in zip(plot_data, legends):
+        plt.plot(dat[:,0], x, label=n)
+    plt.legend()
+    plt.savefig(datfile.replace(".dat", ".S.png"))
+
+
 for d in dat_files:
     make_plot(d, names)
+    make_summarized_plot(d, names)
 
