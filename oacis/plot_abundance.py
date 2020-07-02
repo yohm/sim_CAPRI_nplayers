a996dc8023b52748093a299bb0af3673e22924a5
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-07-02 14:38:46 +0900

    plot N-dependency

--- oacis/plot_abundance.py
+++ oacis/plot_abundance.py
@@ -11,10 +11,33 @@ names = []
 with open("species.txt") as f:
     names = [a.strip() for a in f.readlines() if len(a.strip()) > 0]
 
-pprint.pprint(names)
+#pprint.pprint(names)
 
-dat_files = sorted(glob.glob("abundance_*.dat"))
-pprint.pprint(dat_files)
+types = ["m=1 defensible", "m=1 efficient", "m=1 other"]
+def species_type(name):
+    if re.search(r'/\d_D$', name):
+        return 0
+    elif re.search(r'/\d_E$', name):
+        return 1
+    elif re.search(r'/\d$', name):
+        return 2
+    else:
+        return -1
+
+for n in names:
+    if species_type(n) < 0:
+        types.append(n)
+
+with open("species_types.txt", mode='w') as f:
+    f.write('\n'.join(types))
+
+pprint.pprint(types)
+
+def _get_N(fname):
+    return int(re.search(r'\d+', fname).group(0))
+
+dat_files = sorted(glob.glob("abundance_*.dat"), key = _get_N)
+#pprint.pprint(dat_files)
 
 def make_plot(datfile, names):
     plt.clf()
@@ -29,31 +52,49 @@ def make_plot(datfile, names):
     plt.legend()
     plt.savefig(datfile.replace(".dat", ".png"))
 
-def make_summarized_plot(datfile, names):
+def make_summarized_plot(datfile, names, types):
     plt.clf()
     dat = np.loadtxt(datfile)
     plt.xlabel('benefit')
     plt.ylabel('abundance')
     plt.ylim(0.0, 1.0)
-    legends = ["m=1 defensible", "m=1 efficient", "m=1 other"]
-    plot_data = [np.zeros_like(dat[:,1]), np.zeros_like(dat[:,1]), np.zeros_like(dat[:,1])]
+    betas = dat[:,0]
+    plot_data = [np.zeros_like(dat[:,1]) for t in types]
     for i,name in enumerate(names):
-        if re.search(r'/\d_D$', name):
-            plot_data[0] += dat[:,i+1]
-        elif re.search(r'/\d_E$', name):
-            plot_data[1] += dat[:,i+1]
-        elif re.search(r'/\d$', name):
-            plot_data[2] += dat[:,i+1]
+        t = species_type(name)
+        if t >= 0:
+            plot_data[t] += dat[:, i+1]
         else:
-            plot_data.append(dat[:,i+1])
-            legends.append(name)
-    for x,n in zip(plot_data, legends):
-        plt.plot(dat[:,0], x, label=n)
+            idx = types.index(name)
+            plot_data[idx] = dat[:,i+1]
+    for x,t in zip(plot_data, types):
+        plt.plot(betas, x, label=t)
     plt.legend()
-    plt.savefig(datfile.replace(".dat", ".S.png"))
+    plt.savefig('S_' + datfile.replace(".dat", ".png"))
+    d = np.array([betas] + plot_data).T
+    return d
 
+
+s_data = []
+ns = [_get_N(fname) for fname in dat_files]
 
 for d in dat_files:
     make_plot(d, names)
-    make_summarized_plot(d, names)
+    sd = make_summarized_plot(d, names, types)
+    np.savetxt('S_' + d, sd)
+    s_data.append(sd)
 
+def make_N_dependency_plot(beta_idx, s_data, types, fname):
+    plt.clf()
+    n_dep = s_data[:,beta_idx,:]
+    n_dep[:,0] = ns
+    pprint.pprint(n_dep)
+    for i,t in enumerate(types):
+        plt.plot(ns, n_dep[:,i+1], label=t)
+    plt.legend()
+    plt.savefig(fname+".png")
+    np.savetxt(fname+".dat", n_dep)
+
+s_data = np.array(s_data)
+for idx,fname in zip([19,39,59],['N_dep_beta_2','N_dep_beta_3','N_dep_beta_4']):
+    make_N_dependency_plot(idx, s_data, types, fname)
