2898b1be699a0d63c45d60a5a4b8b3c48b12fbac
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-07-06 06:19:48 +0900

    add script for n=2 evo simulation

---
+++ oacis/run_n2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+$script_dir/../cpp/main_evo_n2 $1 $2 $3 $4 $5 $6 $7 $8 $9
+
+export PIPENV_PIPFILE=$script_dir/Pipfile
+pipenv run python $script_dir/plot_abundance.py
+pipenv run python $script_dir/plot_cooperation_level_n2.py
